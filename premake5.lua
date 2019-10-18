b8a383d52b5f0295007701b7ee903ec516323280
Wallhalla <fabian@sonczeck.de>
2019-10-18 11:28:57 +0200

    GLEW/GLFW/GLM eingebunden

--- premake5.lua
+++ premake5.lua
@@ -1,25 +1,36 @@
 -- premake5.lua
 workspace "HelloOpenGl"
-   configurations { "Debug", "Release" }
+	configurations { "Debug", "Release" }
 
 project "Application"
-   location "Application"		
-   kind "ConsoleApp"
-   language "C++"
-   architecture "x86_64"
-   systemversion "latest"
+	location "Application"		
+	kind "ConsoleApp"
+	language "C++"
+	architecture "x86_64"
+	systemversion "latest"
    
-   targetdir "bin/%{cfg.buildcfg}-%{cfg.architecture}"
+	targetdir "bin/%{cfg.buildcfg}-%{cfg.architecture}"
 
-   files { "**.h", "**.cpp" }
+	files { "**.h", "**.cpp" }
+	
+	includedirs {"ThirdParty/GLEW/include", "ThirdParty/GLFW/include", "ThirdParty/GLM"}
+	libdirs { "ThirdParty/GLEW/lib/Release/x64"}
+	
+	if _ACTION == "vs2019" then
+		libdirs { "ThirdParty/GLFW/lib-vc2019"}
+	elseif _ACTION == "vs2017" then
+		libdirs { "ThirdParty/GLFW/lib-vc2017"}
+	end
+	
+	links { "glfw3", "glew32s", "opengl32"}
 
-filter "system:windows"
+	filter "system:windows"
 		defines {"WINDOWS"}
 
-filter "configurations:Debug"
+	filter "configurations:Debug"
 		defines { "DEBUG" }
 		symbols "On"
 
-filter "configurations:Release"
+	filter "configurations:Release"
 		defines { "NDEBUG" }
 		optimize "On"
